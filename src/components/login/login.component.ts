1619cabb17bccb69fd73814bd2eb0dd67bde2728
Matteo Faccetta <MATTEO.FACCETTA@local.ittcesena.it>
2024-12-02 10:59:08 +0100

    aggiunte cose

---
+++ src/components/login/login.component.ts
@@ -0,0 +1,16 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button'
+
+@Component({
+  selector: 'app-login',
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
