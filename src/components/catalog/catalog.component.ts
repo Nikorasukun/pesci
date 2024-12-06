1eebd12c85009c2abb04a426302f7de57d956787
Matteo <matteo.faccetta.stud@ispascalcomandini.it>
2024-12-06 14:20:05 +0100

    creato concetto del catalogo, da sistemare. problem con l'injection

--- src/components/catalog/catalog.component.ts
+++ src/components/catalog/catalog.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { ServiziService } from '../../services/servizi.service';
 
 @Component({
   selector: 'app-catalog',
-  imports: [],
+  imports: [MatButtonModule, MatCardModule, ServiziService],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css'
 })
 export class CatalogComponent {
-
+  pesci = inject(ServiziService)
 }
