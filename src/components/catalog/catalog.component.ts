0b2bf2614a429d1f6b269ead9cafe10a9343711d
Matteo Faccetta <MATTEO.FACCETTA@local.ittcesena.it>
2024-12-09 10:37:44 +0100

    creata base del catalogo

--- src/components/catalog/catalog.component.ts
+++ src/components/catalog/catalog.component.ts
@@ -1,4 +1,5 @@
-import { Component, inject, Inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Component, HostListener, inject, Inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
@@ -7,11 +8,27 @@ import { ServiziService } from '../../services/servizi.service';
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule, CommonModule],
+  imports: [MatButtonModule, MatCardModule, CommonModule, MatIconModule],
   templateUrl: './catalog.component.html',
-  styleUrl: './catalog.component.css'
+  styleUrl: './catalog.component.css',
 })
 export class CatalogComponent {
-  pesci = inject(ServiziService)
+  pesci = inject(ServiziService);
+
+  colonne: number = 1;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.updateGridCols(event.target.innerWidth);
+  }
+
+  updateGridCols(width: number) {
+    if (width < 600) {
+      this.colonne = 1;
+    } else if (width < 960) {
+      this.colonne = 2;
+    } else {
+      this.colonne = 3;
+    }
+  }
 }
-  
