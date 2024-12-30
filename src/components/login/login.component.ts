38a14ef9d6129122a60f080d14ced764283d5d92
matteo.faccetta.stud <matteo.faccetta.stud@ispascalcomandini.it>
2024-12-30 15:05:13 +0100

    aggiunta gestione login, da creare carrello

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -1,8 +1,12 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import { routes } from './../../app/app.routes';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button'
+import { Signal } from '@angular/core';
+import { ServiziService } from '../../services/servizi.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,5 +16,11 @@ import {MatButtonModule} from '@angular/material/button'
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor(private router:Router){}
+  sessione = inject(ServiziService)
 
+  logAccount(){
+    this.sessione.isLogged.set(true)
+    this.router.navigate(['/catalogo'])
+  }
 }
--- src/components/site-navbar/site-navbar.component.ts
+++ src/components/site-navbar/site-navbar.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-
+import { Component, inject } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { signal } from '@angular/core';
+import { ServiziService } from '../../services/servizi.service';
 
 @Component({
   selector: 'app-site-navbar',
-  imports: [MatButtonModule, MatIconModule, MatToolbarModule, RouterLink, RouterLinkActive],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './site-navbar.component.html',
-  styleUrl: './site-navbar.component.css'
+  styleUrl: './site-navbar.component.css',
 })
-
 export class SiteNavbarComponent {
+  constructor(private router: Router) {}
+  sessione = inject(ServiziService);
+  mostraLogin() {
+    this.router.navigate(['/login']);
+  }
 
+  logout() {
+    this.sessione.isLogged.set(false);
+  }
 }
