fa98648d47408444b56bcee53402094f17b027a4
Matteo <matteo.faccetta.stud@ispascalcomandini.it>
2025-01-20 10:56:57 +0100

    fatta connessione api

---
+++ restapi/api.js
@@ -0,0 +1,40 @@
+var Db = require('./dbcrud');
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var app = express();
+var router = express.Router();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cors());
+app.use('api', router);
+
+router.use((request, response, next)=>{{
+    console.log('Server is running...');
+    next();
+}})
+
+router.route('/clienti').get(
+    (request, response) => {
+        Db.getCustomers().then(
+            (data) => {
+                response.json(data[0])
+            }
+        )
+    }
+)
+
+router.route('/clienti/:id').get(
+    (request, response) => {
+        Db.getCustomer(request.params.id)
+        .then((data) => {
+            response.json(data[0])
+        })
+    }
+)
+
+var port = process.env.PORT || 3000
+app.listen(port)
+console.log('APIs are listening on port')
