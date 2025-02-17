44197dcace1abd2edcbf41ad5bd3d879599d2d23
Nikolas Panterini <NIKOLAS.PANTERINI@local.ittcesena.it>
2025-02-17 10:54:24 +0100

    errore dinamico, ho scritto come potrei fare in servizi.service.ts

--- src/services/servizi.service.ts
+++ src/services/servizi.service.ts
@@ -1,65 +1,50 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, signal, inject, OnInit } from '@angular/core';
 import { Ordine, Sessione, Pesce } from '../classes/session';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
+
 export class ServiziService {
-  constructor() {}
+  constructor() {
+    console.log('entered in ngOnInit by constructor')
+  }
+
+  http = inject(HttpClient);
+
+  FillCatalog(){
+    let listaPesci:Pesce[] = [];
+    this.http.get("http://localhost:8089/api/pesci").subscribe(
+      {
+        next: pesce => {
+          listaPesci = JSON.stringify(pesce) as unknown as Pesce[]; //do il risultato giusto
+          console.log(`${listaPesci}`)
+        },
+        error: error => {
+          console.error("error fetching data ", error)
+        }
+      }
+    )
+    this.sessione.catalogo = listaPesci;
+    return listaPesci;
+  }
+
+  //secondo me il problema è che sessione.catalogo è in realtà una lista
+  //di Ordine, o almeno viene considerato così.
+  //invece io ho creato un metodo che lo rende una lista di Pesci
+  //se potessi sistemare il metodo per far sì che torni una lista di
+  //ordini anziché una lista di pesci, accompagnando quindi quello che
+  //già ho della lista pesci, una quantità.
+  //in tal modo forse potrebbe tutto funzionare come dovrebbe.
+  //non posso cambiare il tipo di catalogo perché dovrei cambiare anche
+  //tutte le volte che lo istanzio e che vado comunque a leggere i dati
+  //da esso e sembra più impegnativo.
 
   isLogged = signal<Boolean>(false);
   cartLenght = signal<number>(0);
   sessione: Sessione = {
-    catalogo: [
-      {
-        id: 0,
-        nome: 'Pesce mandarino',
-        acqua: 'salata',
-        colore: 'bianco,arancione',
-        lunghezza: 0.3,
-        immagine: 'mandarino.webp',
-      },
-      {
-        id: 1,
-        nome: 'Centropyge loricula',
-        acqua: 'salata',
-        colore: 'rosso',
-        lunghezza: 0.2,
-        immagine: 'centropyge.webp',
-      },
-      {
-        id: 2,
-        nome: 'Pesce pappagallo',
-        acqua: 'salata',
-        colore: 'bianco,rosa',
-        lunghezza: 0.4,
-        immagine: 'pappagallo.webp',
-      },
-      {
-        id: 3,
-        nome: 'Pesce Discus',
-        acqua: 'dolce',
-        colore: 'rosso, arancio, marrone',
-        lunghezza: 0.3,
-        immagine: 'discus.jpg',
-      },
-      {
-        id: 4,
-        nome: 'Pesce Killifish',
-        acqua: 'dolce',
-        colore: 'bianco,rosa',
-        lunghezza: 0.4,
-        immagine: 'killifish.jpg',
-      },
-      {
-        id: 5,
-        nome: 'Pesce Betta',
-        acqua: 'dolce',
-        colore: 'bianco,rosa',
-        lunghezza: 0.3,
-        immagine: 'betta.jpg',
-      },
-    ],
+    catalogo: this.FillCatalog(),
   };
   carrello = signal<Ordine[]>(
     this.sessione.catalogo.map((pesce) => new Ordine(pesce, 0))
